9766b5d3974d7576e05189a3ea0a7973f553dda4
alimi <swarthik729@gmail.com>
2021-10-20 00:01:50 +0530

    adding split data file

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,3 @@
-import os
 from get_data import read_params, get_data
 import argparse
 
@@ -11,7 +10,7 @@ def load_and_save(config_path):
     df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
