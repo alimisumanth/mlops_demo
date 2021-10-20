26a1d847d2174e08baff3cb013a39bbf7121d4a2
alimi <swarthik729@gmail.com>
2021-10-20 12:33:57 +0530

    Local package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import  setup, find_packages
+
+
+setup(
+    name='src',
+    version='1.0',
+    description='mlops demo',
+    author='ASK',
+    packages=find_packages(),
+    liscence='MIT',
+
+)
